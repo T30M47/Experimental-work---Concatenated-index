9d6fed6372c6efcaa26c686a70b136c6d7fabacc
T30M47 <teo.matijasic@student.uniri.hr>
2023-12-08 20:29:41 +0100

    Uređen Homepage s dodanim gumbovima (s i bez indx)

--- main/urls.py
+++ main/urls.py
@@ -5,6 +5,8 @@ from . import views
 
 urlpatterns = [
     path('', views.homepage, name='homepage'),
+    path('testWithoutIndex/', testWithoutIndex, name='testWithoutIndex'),
+    path('testWithIndex/', testWithIndex, name = 'testWithIndex'),
     path('withoutIndex/', GetDataWithoutIndex.as_view(), name='get_data_without'),
     path('withIndex/', GetDataWithIndex.as_view(), name = 'get_data_with'),
     path('withPartIndex/', GetDataWithPartIndex.as_view(), name = 'get_data_with_part'),
--- main/views.py
+++ main/views.py
@@ -2,12 +2,14 @@ from django.shortcuts import render
 from django.http import JsonResponse
 from django.views import View
 from .models import *
+import time
 from django.shortcuts import render
 from django.http import HttpResponse
+import subprocess
 # Create your views here.
 
 def homepage(request):
-    return HttpResponse('<h1>Testing read request response time with and without concatenated index.</h1>')
+    return render(request, 'template.html')
 
 class GetDataWithoutIndex(View):
     def get(self, request):
@@ -68,3 +70,43 @@ class GetDataWithWrongIndex(View):
 
         proizvodi_json = list(proizvodi.values())
         return JsonResponse(proizvodi_json, safe=False)
+
+def testWithoutIndex(request):
+    naziv = request.GET.get('naziv', '')
+    cijena = request.GET.get('cijena', '')
+    datum_kreiranja = request.GET.get('datum_kreiranja', '')
+    
+    url = f"http://localhost:8000/withoutIndex/?naziv={naziv}&cijena={cijena}&datum_kreiranja={datum_kreiranja}"
+    result = subprocess.run(['ab', '-n', '1000', '-c', '3', url], capture_output=True, text=True)
+    output = result.stdout.splitlines()
+    
+    response_data = {
+        'parameters': {
+            'naziv': naziv,
+            'cijena': cijena,
+            'datum_kreiranja': datum_kreiranja,
+        },
+        'output': output,
+    }
+    
+    return JsonResponse(response_data)
+
+def testWithIndex(request):
+    naziv = request.GET.get('naziv', '')
+    cijena = request.GET.get('cijena', '')
+    datum_kreiranja = request.GET.get('datum_kreiranja', '')
+    
+    url = f"http://localhost:8000/withIndex/?naziv={naziv}&cijena={cijena}&datum_kreiranja={datum_kreiranja}"
+    result = subprocess.run(['ab', '-n', '1000', '-c', '3', url], capture_output=True, text=True)
+    output = result.stdout.splitlines()
+    
+    response_data = {
+        'parameters': {
+            'naziv': naziv,
+            'cijena': cijena,
+            'datum_kreiranja': datum_kreiranja,
+        },
+        'output': output,
+    }
+    
+    return JsonResponse(response_data)
