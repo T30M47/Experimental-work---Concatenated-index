fb42b06e476c34e484df38458326883b8573631d
T30M47 <teo.matijasic@student.uniri.hr>
2023-12-11 20:27:25 +0100

    Dodane novi URL - ovi.

--- main/urls.py
+++ main/urls.py
@@ -9,8 +9,19 @@ urlpatterns = [
     path('testWithIndex/', testWithIndex, name = 'testWithIndex'),
     path('testWithPartIndex/', testWithPartIndex, name = 'testWithPartIndex'),
     path('testWithWrongIndex/', testWithWrongIndex, name = 'testWithWrongIndex'),
+    path('testWithoutIndexLessRows/', testWithoutIndexLessRows, name = 'testWithoutIndexLessRows'),
+    path('testWithIndexLessRows/', testWithIndexLessRows, name = 'testWithIndexLessRows'),
+    path('testWithIndexBigCard/', testWithIndexBigCard, name = 'testWithIndexBigCard'),
+    path('testWithPartIndexBigCard/', testWithPartIndexBigCard, name = 'testWithPartIndexBigCard'),
+    path('testWithWrongIndexBigCard/', testWithWrongIndexBigCard, name = 'testWithWrongIndexBigCard'),
+
     path('withoutIndex/', GetDataWithoutIndex.as_view(), name='get_data_without'),
     path('withIndex/', GetDataWithIndex.as_view(), name = 'get_data_with'),
     path('withPartIndex/', GetDataWithPartIndex.as_view(), name = 'get_data_with_part'),
-    path('withWrongIndex/', GetDataWithWrongIndex.as_view(), name = 'get_data_with_wrong') 
+    path('withWrongIndex/', GetDataWithWrongIndex.as_view(), name = 'get_data_with_wrong'),
+    path('withoutIndexLessRows/', GetDataWithoutIndexLessRows.as_view(), name = 'get_data_without_less'),
+    path('withIndexLessRows/', GetDataWithIndexLessRows.as_view(), name = 'get_data_with_less'),
+    path('withIndexBigCard/', GetDataWithIndexBigCard.as_view(), name = 'get_data_with_big'),
+    path('withPartIndexBigCard/', GetDataWithPartIndexBigCard.as_view(), name = 'get_data_with_part_big'),
+    path('withWrongIndexBigCard/', GetDataWithWrongIndexBigCard.as_view(), name = 'get_data_with_wrong_big')
 ]
--- main/views.py
+++ main/views.py
@@ -71,6 +71,82 @@ class GetDataWithWrongIndex(View):
         proizvodi_json = list(proizvodi.values())
         return JsonResponse(proizvodi_json, safe=False)
 
+
+class GetDataWithoutIndexLessRows(View):
+    def get(self, request):
+        naziv = request.GET.get('naziv', '')
+        cijena = request.GET.get('cijena', '')
+        datum_kreiranja = request.GET.get('datum_kreiranja', '')
+
+        proizvodi = Proizvod_KriviIndeks.objects.filter(
+            naziv = naziv,
+            cijena = cijena,
+            datum_kreiranja = datum_kreiranja
+        )
+
+        proizvodi_json = list(proizvodi.values())
+        return JsonResponse(proizvodi_json, safe=False)
+
+class GetDataWithIndexLessRows(View):
+    def get(self, request):
+        naziv = request.GET.get('naziv', '')
+        cijena = request.GET.get('cijena', '')
+        datum_kreiranja = request.GET.get('datum_kreiranja', '')
+
+        proizvodi = Proizvod_KriviIndeks.objects.filter(
+            naziv = naziv,
+            cijena = cijena,
+            datum_kreiranja = datum_kreiranja
+        )
+
+        proizvodi_json = list(proizvodi.values())
+        return JsonResponse(proizvodi_json, safe=False)
+
+class GetDataWithIndexBigCard(View):
+    def get(self, request):
+        naziv = request.GET.get('naziv', '')
+        cijena = request.GET.get('cijena', '')
+        datum_kreiranja = request.GET.get('datum_kreiranja', '')
+
+        proizvodi = Proizvod_KriviIndeks.objects.filter(
+            naziv = naziv,
+            cijena = cijena,
+            datum_kreiranja = datum_kreiranja
+        )
+
+        proizvodi_json = list(proizvodi.values())
+        return JsonResponse(proizvodi_json, safe=False)
+
+class GetDataWithPartIndexBigCard(View):
+    def get(self, request):
+        naziv = request.GET.get('naziv', '')
+        cijena = request.GET.get('cijena', '')
+        datum_kreiranja = request.GET.get('datum_kreiranja', '')
+
+        proizvodi = Proizvod_KriviIndeks.objects.filter(
+            naziv = naziv,
+            cijena = cijena,
+            datum_kreiranja = datum_kreiranja
+        )
+
+        proizvodi_json = list(proizvodi.values())
+        return JsonResponse(proizvodi_json, safe=False)
+
+class GetDataWithWrongIndexBigCard(View):
+    def get(self, request):
+        naziv = request.GET.get('naziv', '')
+        cijena = request.GET.get('cijena', '')
+        datum_kreiranja = request.GET.get('datum_kreiranja', '')
+
+        proizvodi = Proizvod_KriviIndeks.objects.filter(
+            naziv = naziv,
+            cijena = cijena,
+            datum_kreiranja = datum_kreiranja
+        )
+
+        proizvodi_json = list(proizvodi.values())
+        return JsonResponse(proizvodi_json, safe=False)
+
 def testWithoutIndex(request):
     naziv = request.GET.get('naziv', '')
     cijena = request.GET.get('cijena', '')
@@ -132,6 +208,106 @@ def testWithPartIndex(request):
     return JsonResponse(response_data)
 
 def testWithWrongIndex(request):
+    naziv = request.GET.get('naziv', '')
+    cijena = request.GET.get('cijena', '')
+    datum_kreiranja = request.GET.get('datum_kreiranja', '')
+    
+    url = f"http://localhost:8000/withWrongIndex/?naziv={naziv}&cijena={cijena}&datum_kreiranja={datum_kreiranja}"
+    result = subprocess.run(['ab', '-n', '1000', '-c', '3', url], capture_output=True, text=True)
+    output = result.stdout.splitlines()
+    
+    response_data = {
+        'parameters': {
+            'naziv': naziv,
+            'cijena': cijena,
+            'datum_kreiranja': datum_kreiranja,
+        },
+        'output': output,
+    }
+    
+    return JsonResponse(response_data)
+
+def testWithoutIndexLessRows(request):
+    naziv = request.GET.get('naziv', '')
+    cijena = request.GET.get('cijena', '')
+    datum_kreiranja = request.GET.get('datum_kreiranja', '')
+    
+    url = f"http://localhost:8000/withWrongIndex/?naziv={naziv}&cijena={cijena}&datum_kreiranja={datum_kreiranja}"
+    result = subprocess.run(['ab', '-n', '1000', '-c', '3', url], capture_output=True, text=True)
+    output = result.stdout.splitlines()
+    
+    response_data = {
+        'parameters': {
+            'naziv': naziv,
+            'cijena': cijena,
+            'datum_kreiranja': datum_kreiranja,
+        },
+        'output': output,
+    }
+    
+    return JsonResponse(response_data)
+
+def testWithIndexLessRows(request):
+    naziv = request.GET.get('naziv', '')
+    cijena = request.GET.get('cijena', '')
+    datum_kreiranja = request.GET.get('datum_kreiranja', '')
+    
+    url = f"http://localhost:8000/withWrongIndex/?naziv={naziv}&cijena={cijena}&datum_kreiranja={datum_kreiranja}"
+    result = subprocess.run(['ab', '-n', '1000', '-c', '3', url], capture_output=True, text=True)
+    output = result.stdout.splitlines()
+    
+    response_data = {
+        'parameters': {
+            'naziv': naziv,
+            'cijena': cijena,
+            'datum_kreiranja': datum_kreiranja,
+        },
+        'output': output,
+    }
+    
+    return JsonResponse(response_data)
+
+def testWithIndexBigCard(request):
+    naziv = request.GET.get('naziv', '')
+    cijena = request.GET.get('cijena', '')
+    datum_kreiranja = request.GET.get('datum_kreiranja', '')
+    
+    url = f"http://localhost:8000/withWrongIndex/?naziv={naziv}&cijena={cijena}&datum_kreiranja={datum_kreiranja}"
+    result = subprocess.run(['ab', '-n', '1000', '-c', '3', url], capture_output=True, text=True)
+    output = result.stdout.splitlines()
+    
+    response_data = {
+        'parameters': {
+            'naziv': naziv,
+            'cijena': cijena,
+            'datum_kreiranja': datum_kreiranja,
+        },
+        'output': output,
+    }
+    
+    return JsonResponse(response_data)
+
+def testWithPartIndexBigCard(request):
+    naziv = request.GET.get('naziv', '')
+    cijena = request.GET.get('cijena', '')
+    datum_kreiranja = request.GET.get('datum_kreiranja', '')
+    
+    url = f"http://localhost:8000/withWrongIndex/?naziv={naziv}&cijena={cijena}&datum_kreiranja={datum_kreiranja}"
+    result = subprocess.run(['ab', '-n', '1000', '-c', '3', url], capture_output=True, text=True)
+    output = result.stdout.splitlines()
+    
+    response_data = {
+        'parameters': {
+            'naziv': naziv,
+            'cijena': cijena,
+            'datum_kreiranja': datum_kreiranja,
+        },
+        'output': output,
+    }
+    
+    return JsonResponse(response_data)
+
+def testWithWrongIndexBigCard(request):
     naziv = request.GET.get('naziv', '')
     cijena = request.GET.get('cijena', '')
     datum_kreiranja = request.GET.get('datum_kreiranja', '')
